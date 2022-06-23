a81b3c510e0fd228d011c203c989379960fc69ce
houbb <houbb@neocrm.com>
2022-06-23 18:47:41 +0800

    fix: 修改调试命令

--- bin/cli.js
+++ bin/cli.js
@@ -7,22 +7,21 @@ const run = (argv) => {
     const projectDir = resolve(__dirname, '../../doch');
     // 删除 node_modules
     execSync(`rm -rf ${projectDir}/node_modules`);
+    console.log(`rm -rf ${projectDir}/node_modules`);
     // 拷贝当前项目到新的目录
     execSync(`cp -r ${projectDir} .doch`);
+    console.log(`cp -r ${projectDir} .doch`);
     process.chdir(`${process.cwd()}/.doch/`);
-    const result = execSync(`npm i`);
-    result.on('data', (chunk) => {
-      console.log(chunk.toString())
-    })
-  } else if (argv[2] === 'dev') {
-    process.chdir(`${process.cwd()}/.doch/`);
-    const result = execSync(`npm run dev`, {
+    execSync(`npm i --registry=https://registry.npmmirror.com`, {
       stdio: 'inherit',
       shell: process.platform === 'win32'
     });
-    result.on('data', (chunk) => {
-      console.log(chunk.toString())
-    })
+  } else if (argv[2] === 'dev') {
+    process.chdir(`${process.cwd()}/.doch/`);
+    execSync(`npm run dev`, {
+      stdio: 'inherit',
+      shell: process.platform === 'win32'
+    });
   } else {
     console.log(`unsupport ${argv[2]}`);
   }
